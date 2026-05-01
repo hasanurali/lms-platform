d8abe1db36e98cdbb66ea1975361a129c0a436da
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-05-01 09:41:08 +0530

    feat: implement global error handling middleware for centralized error responses

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
+import errorHandler from "./middlewares/error.middleware.js";
 
 const app = express();
 
-// middleware
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
 
-// test route
+// Test route
 app.get("/", (req, res) => {
     res.json({
         success: true,
@@ -16,9 +18,15 @@ app.get("/", (req, res) => {
     });
 });
 
-// not found route
+// Not found route
 app.use((req, res) => {
-    res.status(404).json({ message: "Route not found" });
+    res.status(404).json({
+        success: false,
+        message: "Route not found"
+    });
 });
+
+// Globle error handler
+app.use(errorHandler);
 
 export default app;
