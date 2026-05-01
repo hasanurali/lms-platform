9368a44bc5dbde2e5c851f8687660fa65975f8f2
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-05-01 20:16:06 +0530

    chore: add cookie configuration

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,5 +1,6 @@
 import "dotenv/config"
 import JWT_CONFIG from "./jwt.config.js"
+import COOKIE_CONFIGURATION from "./cookie.config.js";
 
 // Validate required env
 const required = ["MONGO_URI", "JWT_REFRESH_KEY", "JWT_ACCESS_KEY"];
@@ -10,9 +11,17 @@ required.forEach((key) => {
 });
 
 export const config = Object.freeze({
+
     env: process.env.NODE_ENV || 'production',
+
     port: parseInt(process.env.PORT, 10) || 2000,
+
     mongoUri: process.env.MONGO_URI,
+
     bcryptRounds: Number(process.env.BCRYPT_SALT_ROUNDS) || 10,
+
     jwt: JWT_CONFIG,
+
+    cookie: COOKIE_CONFIGURATION
+
 });
