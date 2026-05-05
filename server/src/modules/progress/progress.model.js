debbb348e35d42452602e6181cfd3781a489aaa8
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-05-05 10:44:22 +0530

    feat: add progress model

---
+++ server/src/modules/progress/progress.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+
+const progressSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    course: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+        required: true
+    },
+    completedLessons: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Lesson"
+        }
+    ],
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    lastAccessLesson: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lesson",
+    }
+}, { timestamps: true });
+
+// Add index for faster query
+progressSchema.index({ user: 1, course: 1 }, { unique: true });
+
+export default mongoose.model("Progress", progressSchema);
