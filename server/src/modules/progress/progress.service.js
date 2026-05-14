edb3e4882f2d0a062c24355d98b780da64a0651d
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-05-14 10:46:17 +0530

    perf: improve progress service performance

--- server/src/modules/progress/progress.service.js
+++ server/src/modules/progress/progress.service.js
@@ -13,24 +13,29 @@ export const getProgressService = async (courseId, userId) => {
     // Validate ID
     validateObjectId(courseId);
 
+
+    // Parallelize course and enrollment existance check
+    const [course, enrolled] = await Promise.all([
+        courseModel.exists({ _id: courseId }),
+        enrollmentModel.exists({ user: userId, course: courseId })
+    ]);
+
     // Check course exists
-    const course = await courseModel.exists({ _id: courseId });
     if (!course) {
         throw new ApiError(HTTP_STATUS.NOT_FOUND, MESSAGES.COURSE.NOT_FOUND);
     };
 
     // Check user enrolled in this course
-    const isEnrolled = await enrollmentModel.exists({ user: userId, course: courseId });
-    if (!isEnrolled) {
+    if (!enrolled) {
         throw new ApiError(HTTP_STATUS.FORBIDDEN, MESSAGES.ENROLLMENT.REQUIRED)
     };
 
-    // Get modules
-    const modules = await moduleModel.find({ course: courseId }).select("_id");
-    const moduleIds = modules.map(m => m._id);
-
-    // Get progress
-    const progress = await progressModel.findOne({ user: userId, course: courseId });
+    const [moduleIds, progress] = await Promise.all([
+        moduleModel.distinct("_id", { course: courseId }),
+        progressModel.findOne({ user: userId, course: courseId })
+            .select("_id course user completedLessons completed lastAccessLesson")
+            .lean()
+    ]);
 
     // Count total lessons
     const totalLessons = await lessonModel.countDocuments({
@@ -65,58 +70,65 @@ export const completeLessonService = async ({ courseId, lessonId, userId }) => {
     validateObjectId(courseId);
     validateObjectId(lessonId);
 
+    // Parallelize course and enrollment existance check
+    const [course, enroll] = await Promise.all([
+        courseModel.exists({ _id: courseId }),
+        enrollmentModel.exists({ user: userId, course: courseId })
+    ]);
+
     // Check course exists
-    const course = await courseModel.exists({ _id: courseId });
     if (!course) {
         throw new ApiError(HTTP_STATUS.NOT_FOUND, MESSAGES.COURSE.NOT_FOUND);
     };
 
     // Check user enrolled in this course
-    const isEnrolled = await enrollmentModel.exists({ user: userId, course: courseId });
-    if (!isEnrolled) {
+    if (!enroll) {
         throw new ApiError(HTTP_STATUS.FORBIDDEN, MESSAGES.ENROLLMENT.REQUIRED)
     };
 
     // Check if course already completed
-    let progress = await progressModel.findOne({ user: userId, course: courseId });
+    let progress = await progressModel.findOne({ user: userId, course: courseId })
+        .select("_id course user completedLessons completed lastAccessLesson")
+        .lean()
 
     // If course completed return data
     if (progress?.completed) {
         return progress;
     };
 
-    // Get modules
-    const modules = await moduleModel.find({ course: courseId }).select("_id");
-    const moduleIds = modules.map(m => m._id);
+    // Get moduleIds
+    const moduleIds = await moduleModel.distinct("_id", { course: courseId });
 
-    // Count total lessons
-    const totalLessons = await lessonModel.countDocuments({ module: { $in: moduleIds } });
+    // Parallelize count lesson and valid lesson
+    const [totalLessons, validLesson] = await Promise.all([
+        lessonModel.countDocuments({ module: { $in: moduleIds } }),
+        lessonModel.exists({ _id: lessonId, module: { $in: moduleIds } })
+    ]);
 
     // Check lesson belong to this course
-    const isValidLesson = await lessonModel.exists({ _id: lessonId, module: { $in: moduleIds } });
-    if (!isValidLesson) {
-        throw new ApiError(HTTP_STATUS.BAD_REQUEST, MESSAGES.GENERAL.VALIDATION_ERROR)
+    if (!validLesson) {
+        throw new ApiError(HTTP_STATUS.BAD_REQUEST, MESSAGES.LESSON.NOT_IN_COURSE)
     };
 
     // Update progress
-    progress = await progressModel.findOneAndUpdate(
+    const updatedProgress = await progressModel.findOneAndUpdate(
         { user: userId, course: courseId },
         {
             $addToSet: { completedLessons: lessonId },
             $set: { lastAccessLesson: lessonId }
         },
         { upsert: true, new: true }
-    );
+    ).select("_id course user completedLessons completed lastAccessLesson").lean();
 
     // Mark complete if finished
-    const isComplete = totalLessons > 0 && progress.completedLessons.length === totalLessons;
-    if (isComplete && !progress.completed) {
-        progress.completed = true;
-        await progress.save();
+    const isComplete = totalLessons > 0 && updatedProgress.completedLessons.length === totalLessons;
+    if (isComplete && !updatedProgress.completed) {
+        progressModel.findByIdAndUpdate(updatedProgress._id, { completed: true }).exec();
+        updatedProgress.completed = true;
     }
 
     // Return data
-    return progress;
+    return updatedProgress;
 };
 
 export const setLastAccessedLessonService = async ({ courseId, lessonId, userId }) => {
@@ -125,26 +137,29 @@ export const setLastAccessedLessonService = async ({ courseId, lessonId, userId 
     validateObjectId(courseId);
     validateObjectId(lessonId);
 
+    // Parallelize course and enrollment existance check
+    const [course, enroll] = await Promise.all([
+        courseModel.exists({ _id: courseId }),
+        enrollmentModel.exists({ user: userId, course: courseId })
+    ]);
+
     // Check course exists
-    const course = await courseModel.exists({ _id: courseId });
     if (!course) {
         throw new ApiError(HTTP_STATUS.NOT_FOUND, MESSAGES.COURSE.NOT_FOUND);
     };
 
     // Check user enrolled in this course
-    const isEnrolled = await enrollmentModel.exists({ user: userId, course: courseId });
-    if (!isEnrolled) {
+    if (!enroll) {
         throw new ApiError(HTTP_STATUS.FORBIDDEN, MESSAGES.ENROLLMENT.REQUIRED)
     };
 
-    // Get modules
-    const modules = await moduleModel.find({ course: courseId }).select("_id");
-    const moduleIds = modules.map(m => m._id);
+    // Get module ids
+    const moduleIds = await moduleModel.distinct("_id", { course: courseId });
 
     // Check lesson belong to this course
-    const isValidLesson = await lessonModel.exists({ _id: lessonId, module: { $in: moduleIds } });
-    if (!isValidLesson) {
-        throw new ApiError(HTTP_STATUS.BAD_REQUEST, MESSAGES.GENERAL.VALIDATION_ERROR)
+    const validLesson = await lessonModel.exists({ _id: lessonId, module: { $in: moduleIds } });
+    if (!validLesson) {
+        throw new ApiError(HTTP_STATUS.BAD_REQUEST, MESSAGES.LESSON.NOT_IN_COURSE)
     };
 
     // Update last lesson
@@ -154,7 +169,8 @@ export const setLastAccessedLessonService = async ({ courseId, lessonId, userId 
             $set: { lastAccessLesson: lessonId }
         },
         { upsert: true, new: true }
-    );
+    ).select("_id course user completedLessons completed lastAccessLesson").lean();
+
 
     // Return data
     return progress;
