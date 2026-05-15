66c7d74e5d73bf8a275a8a143c090c8d3cdfc4f3
Sk Hasanur Ali <hasanurali807@gmail.com>
2026-05-15 11:41:31 +0530

    feat: add redis TTL constants

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -3,3 +3,4 @@ export { default as MESSAGES } from './message.js';
 export { default as HTTP_STATUS } from './statusCode.js';
 export { default as CLOUDINARY } from "./cloudinary.js"
 export { default as DOUBT_STATUS} from "./doubtStatus.js"
+export {default as REDIS_TTL} from "./redisTtl.js"
